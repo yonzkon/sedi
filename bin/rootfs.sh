abeb80b72d1d4331e4187d0f525529e647691abd
munie <395412693@qq.com>
2016-09-03 14:22:08 +0800

    add rootfs_etc

--- bin/rootfs.sh
+++ bin/rootfs.sh
@@ -22,10 +22,24 @@ build_rootfs()
 {
 	mkdir -p $PREFIX
 	cd $PREFIX
+
+	# /
 	mkdir -p bin sbin lib etc usr var dev proc sys tmp && chmod 1777 tmp
-	mkdir -p etc/init.d etc/udev && touch etc/inittab etc/fstab etc/init.d/rcS
+
+	# /etc
+	mkdir -p etc/init.d etc/udev/rules.d
+	touch etc/inittab etc/fstab etc/profile etc/passwd etc/group
+	touch etc/init.d/rcS etc/udev/rules.d/99-custom.rules
+	chmod +x etc/init.d/rcS
+	chmod 0600 etc/passwd etc/group
+
+	# /usr
 	mkdir -p usr/bin usr/sbin usr/lib usr/include usr/share usr/src
+
+	# /var
 	mkdir -p var/lib var/lock var/log var/run var/tmp && chmod 1777 var/tmp
+
+	# /dev
 	sudo mknod -m 600 dev/mem c 1 1
 	sudo mknod -m 666 dev/null c 1 3
 	sudo mknod -m 666 dev/zero c 1 5
@@ -39,6 +53,11 @@ build_rootfs()
 	ln -sf /proc/self/fd/0 dev/stdin
 	ln -sf /proc/self/fd/1 dev/stdout
 	ln -sf /proc/self/fd/2 dev/stderr
+	for i in $(seq 0 3); do
+		sudo mknod -m 600 dev/mtd$i c 90 $(expr $i + $i)
+		sudo mknod -m 600 dev/mtdblock$i b 31 $i
+	done
+
 	cd -
 }
 
