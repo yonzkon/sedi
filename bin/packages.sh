427d0076476225f422d70f9da2de19a495952d5c
yiendxie <yiendxie@gmail.com>
2017-08-06 10:08:12 +0800

    refactor packages.sh

--- bin/packages.sh
+++ bin/packages.sh
@@ -1,96 +1,72 @@
 #!/bin/bash
 
-    # I) - Usage & Variable
-usage()
-{
-    echo "Usage: {package.sh \$COMMAND \$MANAGER}"
-    echo "Usage: \$COMMAND {base | devel | xorg}"
-    echo "Usage: \$MANAGER {pacman | yum}"
-}
-
 if [ -z "$1" ]; then
-    usage && exit -1
+	usage && exit -1
 else
-    COMMAND=$(tr [A-Z] [a-z] <<<$1);
-    if [ -z "$2" ]; then
-        MANAGER=pacman;
-    else
-        MANAGER=$(tr [A-Z] [a-z] <<<$2);
-    fi
+	COMMAND=$(tr [A-Z] [a-z] <<<$1);
+	if [ -z "$2" ]; then
+		MANAGER=pacman;
+	else
+		MANAGER=$(tr [A-Z] [a-z] <<<$2);
+	fi
 fi
 
 case $MANAGER in
 pacman)
-    UPDATE="-Syu"
-    INSTALL="-S --needed"
-    ;;
+	UPDATE="-Syu"
+	INSTALL="-S --needed"
+	;;
 yum)
-    UPDATE="update -yx'kernel*'"
-    INSTALL="install -y"
-    ;;
+	UPDATE="update -yx'kernel*'"
+	INSTALL="install -y"
+	;;
 *)
-    usage && exit -1
-    ;;
+	usage && exit -1
+	;;
 esac
 
-    # II) - install packages
-    # gcc make vim git gdb clang cmake boost minicom
-    # gentoo-sources grub sudo pciutils usbutils lshw net-tools iputils wpa_supplicant ntp
-    # nmap tcpdump telnet-bsd ftp
-    # clisp perl python lua
-    # xorg-server xinit wqy-zenhei dejavu gtk+extra cinnamon lilyterm firefox adobe-flash
-    # fcitx fcitx-configtool fcitx-sunpinyin evince wps-office gvim emacs
+usage()
+{
+	echo "Usage: {package.sh \$COMMAND \$MANAGER}"
+	echo "Usage: \$COMMAND {base | xorg}"
+	echo "Usage: \$MANAGER {pacman | yum}"
+}
+
+install_base()
+{
+	$MANAGER $UPDATE
+	$MANAGER $INSTALL grub #efibootmgr
+	$MANAGER $INSTALL lshw pciutils usbutils iputils net-tools wpa_supplicant
+	$MANAGER $INSTALL vim git bash zsh sudo
+	$MANAGER $INSTALL nmap tcpdump iptables iproute2 #netcat traceroute dnsutils
+	$MANAGER $INSTALL openssh ntp
+	$MANAGER $INSTALL gcc gdb make minicom
+	#$MANAGER $INSTALL base-devel #autoconf automake bison fakeroot flex m4 pkg-config
+	if [ "$MANAGER" = "pacman" ]; then
+		$MANAGER $INSTALL linux-headers
+	fi
+}
+
+install_xorg()
+{
+	if [ "$MANAGER" = "pacman" ]; then
+		$MANAGER $INSTALL xorg-server xorg-xinit wqy-zenhei ttf-dejavu
+		$MANAGER $INSTALL cinnamon/mate emacs lilyterm wireshark-gtk qemu
+		$MANAGER $INSTALL fcitx fcitx-configtool fcitx-sunpinyin fcitx-gtk2 fcitx-gtk3 fcitx-qt5
+		$MANAGER $INSTALL chromium evince mpv
+	elif [ "$MANAGER" = "yum" ]; then
+		$MANAGER -y groupinstall "X Window System"
+		$MANAGER -y install wqy-zenhei-fonts
+		$MANAGER -y -xNetworkManager* groupinstall gnome
+		$MANAGER -y --enablerepo=RPMForge install ibus-pinyin evince firefox flash-plugin
+	fi
+}
+
+# main
 case $COMMAND in
-base)
-        # 0) - archlinux
-    if [ "$MANAGER" = "pacman" ]; then
-        $MANAGER $UPDATE
-        $MANAGER $INSTALL grub #efibootmgr
-        $MANAGER $INSTALL vim bash zsh pciutils usbutils iputils net-tools wpa_supplicant
-        $MANAGER $INSTALL iptables iproute2 tcpdump nmap #netcat traceroute dnsutils
-        $MANAGER $INSTALL openssh ntp
-        # 1) - CentOS & Fedora
-    elif [ "$MANAGER" = "yum" ]; then
-        $MANAGER $UPDATE
-        $MANAGER $INSTALL grub pciutils usbutils file openssl bc tree lvm2 ntp unzip
-        $MANAGER $INSTALL vim ctags grep sed gawk diffutils patch
-        $MANAGER $INSTALL net-tools wpa_supplicant tcpdump nmap netcat traceroute
-    fi
-    ;;
-devel)
-        # 0) - archlinux
-    if [ "$MANAGER" = "pacman" ]; then
-        $MANAGER $INSTALL git vim ctags gcc clang gdb make cmake minicom
-        $MANAGER $INSTALL base-devel #autoconf automake bison fakeroot flex m4 pkg-config
-        $MANAGER $INSTALL linux-headers boost
-        $MANAGER $INSTALL perl python2 lua nodejs ruby clisp
-        #$MANAGER $INSTALL gcc-objc gnustep-core
-        #$MANAGER $INSTALL jdk maven
-        $MANAGER $INSTALL mariadb
-        # 1) - CentOS & Fedora
-    elif [ "$MANAGER" = "yum" ]; then
-        $MANAGER $INSTALL make gcc gdb bison flex dialog minicom perl python2
-        $MANAGER $INSTALL kernel-devel ncurses-devel bind-utils
-    fi
-    ;;
-xorg)
-        # 0) - archlinux
-    if [ "$MANAGER" = "pacman" ]; then
-        $MANAGER $INSTALL xorg-server xorg-xinit wqy-zenhei ttf-dejavu #xf86-video-intel xf86-video-nouveau
-        $MANAGER $INSTALL cinnamon gvim lilyterm gnome-terminal gnome-mplayer mplayer #alsa-utils smplayer
-        $MANAGER $INSTALL ibus-pinyin evince firefox flashplugin libvdpau
-        #$MANAGER $INSTALL qt4 virtualbox wireshark-gtk eclipse-java netbeans
-        # 1) - CentOS & Fedora
-    elif [ "$MANAGER" = "yum" ]; then
-        $MANAGER -y groupinstall "X Window System"
-        $MANAGER -y install wqy-zenhei-fonts
-        $MANAGER -y -xNetworkManager* groupinstall gnome
-        $MANAGER -y --enablerepo=RPMForge install ibus-pinyin evince firefox flash-plugin
-    fi
-    ;;
-*)
-    usage && exit -1
-    ;;
+base) install_base;;
+xorg) install_xorg;;
+*) usage && exit 1;;
 esac
 
 exit 0
