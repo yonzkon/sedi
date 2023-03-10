c3dd48cf45ce3a5a17564b1beddf5fcfd52284bb
yonzkon <yonzkon@gmail.com>
2023-03-10 08:09:00 +0800

    update tmux-devel.sh

--- bin/tmux-devel.sh
+++ bin/tmux-devel.sh
@@ -2,5 +2,8 @@
 
 tmux new-session -s devel -d
 tmux split-window -v
-tmux resize-pane -D 10
+tmux split-window -h
+tmux selectp -L
+tmux selectp -U
+#tmux resize-pane -D 10
 tmux attach-session -t devel
